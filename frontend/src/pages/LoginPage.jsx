1f20816784f40c210c134b0e6f656d85fd0e7028
ecmatz <112632050+ecmatz@users.noreply.github.com>
2023-02-27 15:20:26 -0500

    Login Screen Styling
    Worked on some styling for the input fields for login screen and added Clarkson Logo

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,16 +1,19 @@
 //This is the login page for our application
 import React, {useContext} from 'react'
 import AuthContext from '../context/AuthContext'
-
+import './styles/loginpage.css'
 const LoginPage = () => {
   let { loginUser } = useContext(AuthContext)
 
   return (
     <div>
         <form onSubmit={loginUser}>
-            <input type='text' name='username' placeholder='Enter Username'></input>
-            <input type='password' name='password' placeholder='Enter Password'></input>
+          <p className='fields'>
+            <img src='https://www.clarkson.edu/sites/default/files/styles/max_325x325/public/media/image/2019-06/shield%20green%20and%20gold_0.png?itok=LczM7Q8j'></img>
+            <input type='text' name='username' placeholder='Your username..'></input>
+            <input type='password' name='password' placeholder='Your password..'></input>
             <input type='submit' />
+            </p>
         </form>
     </div>
   )
