75b0b1daf15de444679642658ae8926a948af59d
paytonshafer <paytonshafer@gmail.com>
2023-02-27 11:39:21 -0500

    added navbar to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage'
 import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Router>
         <AuthProvider>
           <Routes>
