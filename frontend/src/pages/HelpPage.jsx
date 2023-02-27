b1b03f08359c823747de5a5350f93625176c9b53
paytonshafer <paytonshafer@gmail.com>
2023-02-27 14:23:15 -0500

    Created help page

---
+++ frontend/src/pages/HelpPage.jsx
@@ -0,0 +1,12 @@
+//This is the help page it will contain instructions for everything
+import React from 'react';
+
+const HelpPage = () => {
+    return (
+        <div>
+            <p>This is the help page!</p>
+        </div>
+    )
+}
+
+export default HelpPage;
