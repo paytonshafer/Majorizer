7d532c6aa9695636bc8ae9c4b83082f9f0cbda00
paytonshafer <paytonshafer@gmail.com>
2023-02-27 14:24:07 -0500

    Created request page for advisor/stud (view/send)

---
+++ frontend/src/pages/SendRequestPage.jsx
@@ -0,0 +1,12 @@
+//This is the page where a student can send a request to their advisor
+import React from 'react';
+
+const SendRequestPage = () => {
+    return (
+        <div>
+            <p>This is the page where a student can send a request to the advisor!</p>
+        </div>
+    )
+}
+
+export default SendRequestPage;
---
+++ frontend/src/pages/ViewRequestPage.jsx
@@ -0,0 +1,12 @@
+//This is the page where a advisor can see student request and aprove/deny them and leave a comment
+import React from 'react';
+
+const ViewRequestPage = () => {
+    return (
+        <div>
+            <p>This is the page where an advisor can see their requests!</p>
+        </div>
+    )
+}
+
+export default ViewRequestPage;
