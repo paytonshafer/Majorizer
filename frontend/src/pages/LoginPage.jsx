4d2f722fe66c972a701e552fcabd18255f3f79a3
ecmatz <112632050+ecmatz@users.noreply.github.com>
2023-02-27 17:19:22 -0500

    CSS Cleanup
    Just made some slight tweaks to the CSS to try and improve the UI

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -9,10 +9,10 @@ const LoginPage = () => {
     <div>
         <form onSubmit={loginUser}>
           <p className='fields'>
-            <img src='https://www.clarkson.edu/sites/default/files/styles/max_325x325/public/media/image/2019-06/shield%20green%20and%20gold_0.png?itok=LczM7Q8j'></img>
-            <input type='text' name='username' placeholder='Your username..'></input>
-            <input type='password' name='password' placeholder='Your password..'></input>
-            <input type='submit' />
+            <img src='https://www.clarkson.edu/sites/default/files/styles/max_325x325/public/media/image/2019-06/shield%20green%20and%20gold_0.png?itok=LczM7Q8j' alt='Clarkson University Logo'></img>
+            <input type='text' name='username' placeholder='Enter Username'></input>
+            <input type='password' name='password' placeholder='Enter Password'></input>
+            <input type='submit' value='Login'/>
             </p>
         </form>
     </div>
