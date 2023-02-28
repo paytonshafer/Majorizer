c531feb78ff4e296b66062654c134505245d6e00
paytonshafer <paytonshafer@gmail.com>
2023-02-28 13:51:57 -0500

    added separate parts to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,12 +2,30 @@
 import React, {useContext} from 'react'
 import AuthContext from '../context/AuthContext.js'
 
+const StudHome = () =>{
+    return(<div>Hello student!</div>)
+}
+
+const AdvHome = () =>{
+    return(<div>Hello advisor!</div>)
+}
+
+const AdmHome = () =>{
+    return(<div>Hello admin!</div>)
+}
+
 const HomePage = () => {
     let {user} = useContext(AuthContext)
 
     return (
         <div>
             <p>Hello {user.username} you are a {user.group}</p>
+            {/*The below block checks for a user and renders the correct nave bar based on who is logged in*/
+            user === null ? (<h3>WELCOME!</h3>) :
+            user.group === 'student' ? (<StudHome />) :
+            user.group === 'advisor' ? (<AdvHome />) :
+            user.group === 'admin' ? (<AdmHome />) : 
+            null}
         </div>
     )
 }
