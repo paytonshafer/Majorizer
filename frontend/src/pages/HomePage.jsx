b4793f95e75213bbed054e6e7f2b5dc6912bd5b4
ceilidhrose <112649029+ceilidhrose@users.noreply.github.com>
2023-03-02 21:56:22 +0000

    added to advisor and admin HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 //This is the code for our home page
 import React, {useContext} from 'react'
-import {useResolvedPath} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import AuthContext from '../context/AuthContext.js'
 import './styles/HomePage.css'
 
@@ -15,23 +15,30 @@ const StudHome = () =>{
 const AdvHome = () =>{
     return(
     <div>
-       hello there
+       <p className='courseListHeader'>Your current courses:</p>
+        <p className='course1'>CS350: Software Design & Development</p>
+            <p className='course1Time'>TTh 1:30-2:45</p>
+            <p className='course1Location'>Snell 213</p>
+        <p className='course2'>CS459: Human-Computer Interaction</p>
+            <p className='course2Time'>TTh 4:30-5:45</p>
+            <p className='course2Location'>Science Center 348</p>
     </div>)
 }
 
 const AdmHome = () =>{
     return(
     <div>
-        hello there
+        <p className='activeRequestField'>You have 2 active requests!</p>
     </div>)
 }
+//this will be edited to link with backend stuff later on, hardcoding in 2 for rn
 
 const HomePage = () => {
     let {user} = useContext(AuthContext)
 
     return (
         <div>
-            <p className='welcomeField'>Hello {user.username} you are a {user.group}</p>
+            <p className='welcomeField'>Hello {user.username}: {user.group} account</p>
             {/*The below block checks for a user and renders the correct nave bar based on who is logged in*/
             user === null ? (<h3>WELCOME!</h3>) :
             user.group === 'student' ? (<StudHome />) :
