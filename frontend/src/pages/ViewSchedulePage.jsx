b2e8dc9a5b91c7f21024dd156de5cfd3b8e11415
ecmatz <112632050+ecmatz@users.noreply.github.com>
2023-03-04 15:24:26 -0500

    began working on schedule viewer

--- frontend/src/pages/ViewSchedulePage.jsx
+++ frontend/src/pages/ViewSchedulePage.jsx
@@ -1,9 +1,52 @@
 import AuthContext from '../context/AuthContext';
 import React, { useContext } from 'react';
-
+import './styles/ViewSchedulePage.css'
 //student view schedule page
+class ScheduleForm extends React.Component
+{
+    constructor(props){
+        super(props);
+        this.state = {value: 'Schedule 1'}
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        }
+        handleChange(event){
+            this.setState({value: event.target.value});
+        }
+        handleSubmit(event){
+            alert("You chose " + this.state.value);
+            event.preventDefault();
+        }
+        render(){
+            return(
+                <form onSubmit={this.handleSubmit}>
+                <label>
+                  <h2>Choose a schedule:</h2>
+                  <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="S1">Schedule 1</option>
+                    <option value="S2">Schedule 2</option>
+                    <option value="S3">Schedule 3</option>
+                    <option value="S4">Schedule 4</option>
+                    <option value="S5">Schedule 5</option>
+                    <option value="S6">Schedule 6</option>
+                    <option value="S7">Schedule 7</option>
+                    <option value="S8">Schedule 8</option>
+                  </select>
+                </label>
+                <input id='selectbutton' type="submit" value="Select" />
+              </form>
+            );
+        }
+    }
+
 const StudView = () => {
-    return (<p>This is the student schedule viewer</p>)
+    return (
+    <div className='borderbox'>
+    <h1>Welcome to the Schedule Viewer</h1>
+    <ScheduleForm></ScheduleForm>
+    </div>
+    )
 }
 
 //advisor view schedule page
