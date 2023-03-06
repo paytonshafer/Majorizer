7a4f593f0f5adc69221fa10f97d8a43973dc32ad
ecmatz <112632050+ecmatz@users.noreply.github.com>
2023-03-06 14:58:51 -0500

    Started adding changing advisor student pairings

--- frontend/src/pages/ConnectionPage.jsx
+++ frontend/src/pages/ConnectionPage.jsx
@@ -5,6 +5,48 @@ import './styles/ConnectionPage.css'
 /*right now the connections are static hard coded. Later, we can make the connection component into a function and
 have multiple appear. We can also then track each individually by an id and actually let the connectees know if
 the connection was approved or denied */
+class PairingForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {advisor: 'Advisor1', student: 'Student1'}
+        this.handleChangeAdv = this.handleChangeAdv.bind(this);
+        this.handleChangeStu = this.handleChangeStu.bind(this);
+    }
+    handleChangeAdv(e){
+        this.setState({advisor:e.target.value});
+    }
+    handleChangeStu(e){
+        var newAdv = e.target.value;
+        this.setState({student:e.target.value});
+    }
+    render() {
+        var student=this.state.student;
+        var advisor=this.state.advisor;
+        return (
+        <div>
+            <label htmlFor='advselect'>Advisor</label>
+         <select 
+         defaultValue={this.state.advisor} 
+         onChange={this.handleChangeAdv} id = 'advselect'
+         >
+            <option value="Advisor1">Advisor1</option>
+            <option value="Advisor2">Advisor2</option>
+          </select>
+          <label htmlFor='stuselect'>Student</label>
+         <select 
+         defaultValue={this.state.student} 
+         onChange={this.handleChangeStu} id = 'stuselect'
+         >
+            <option value="Student1">Student1</option>
+            <option value="Student2">Student2</option>
+          </select>
+          <p>Make pair {student}, {advisor}? </p>
+    
+          </div>        
+        );
+    }
+}
+
 const ConnectionPage = () => {
     let {user} = useContext(AuthContext);
     function removeConnection(){
@@ -20,7 +62,10 @@ const ConnectionPage = () => {
     return (
         <div>
             <div className='borderbox-2'>
-            <h1>Welcome, {user.username}. Confirm connections below.</h1>
+            <h1>Welcome, {user.username}.</h1>
+            <h3> Change Student/Advisor Pairings:</h3>
+            <PairingForm></PairingForm>
+            <h3> Approve Connections Below:</h3>
             <div id = "connection-1" className='connection'>
                 <div className='connection-header'>
                 <p>Admin: Admin1</p>
