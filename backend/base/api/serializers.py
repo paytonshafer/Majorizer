228a5a496a7c6867398be8c06607b7b977cd7d1d
paytonshafer <paytonshafer@gmail.com>
2023-03-25 13:50:58 -0400

    Create serializer for 4 models

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -1,4 +1,25 @@
 #This is the serializer file, it will have classes that will take a python object to a json object
 #So that they can be sent through the api
 #We will create a serializer for each model
+from rest_framework.serializers import ModelSerializer #A serializer takes a python object to a json object
+from base.models import Student, AdvStudConn, Request, Schedule
 
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class AdvStudConnSerializer(ModelSerializer):
+    class Meta:
+        model = AdvStudConn
+        fields = '__all__'
+
+class RequestSerializer(ModelSerializer):
+    class Meta:
+        model = Request
+        fields = '__all__'
+
+class ScheduleSerializer(ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = '__all__'
